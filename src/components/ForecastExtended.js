e7c52e81f8c1230e7ead97b2082146664d1b39fb
Rodrigo Andrés Góngora Herrera <andres11362@gmail.com>
2020-02-11 03:37:46 -0500

    Ultima practica

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -4,36 +4,58 @@ import ForecastItem from './ForecastItem';
 import './styles.css';
 
 
-const days = [
-    'Lunes',
-    'Martes',
-    'Miercoles',
-    'Jueves',
-    'Viernes'
-]
+// const days = [
+//     'Lunes',
+//     'Martes',
+//     'Miercoles',
+//     'Jueves',
+//     'Viernes'
+// ]
 
-const data = {
-    temperature: 10,
-    humidity: 10,
-    weatherState: 'normal',
-    wind: 'normal'
-}
+// const data = {
+//     temperature: 10,
+//     humidity: 10,
+//     weatherState: 'normal',
+//     wind: 'normal'
+// }
+
+const api_key = '21b0a24a347f39ea5c6d32ef38d56e35';
+const url = 'http://api.openweathermap.org/data/2.5/forecast'
 
 
 class ForecastExtended extends Component {
 
+    constructor(){
+        super();
+        this.state = { forecastData: null }
+    }
+
+    componentDidMount() {
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+        fetch(url_forecast).then(response => (response.json()))
+        .then( weather_data => (console.log(weather_data)))    
+    }
+    
     renderForecastItemDays(){
-        return days.map( day => (<ForecastItem key={ day } weekDay={ day } hour={ 10 } data= { data } ></ForecastItem>));
+        return "Render Items";  
+        //return days.map( day => (<ForecastItem key={ day } weekDay={ day } hour={ 10 } data= { data } ></ForecastItem>));
+    }
+
+    renderProgress(){
+        return 'Cargando pronostico extendido...';
     }
 
     render() {
         const { city } = this.props;
+
+        const { forecastData } = this.state
+
         return (
             <div>
                 <h2 className="forecast-title">
                     Pronostico Extendido para { city }
                 </h2>
-                { this.renderForecastItemDays() }
+                <h3>{ forecastData ? this.renderForecastItemDays() : this.renderProgress() }</h3>
             </div>
         );
     }
